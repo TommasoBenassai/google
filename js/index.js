a3ac0462934ae4d7085eb727407296210a9db689
TOMMASO BENASSAI <72451626+TommasoBenassai@users.noreply.github.com>
2024-03-21 16:40:13 +0100

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,13 @@
+function hello(){
+   // alert("Questa è l' immagine di Edo");
+    $("#Edo").css("height",100);
+    window.open("https://www.google.com");
+}
+function search(){
+    var val=$("#test").val();
+    window.open("https://www.google.com/search?q="+val);
+}
+
+function fortunato() {
+    window.open("https://doodles.google/");
+}
---
+++ test.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<?php
+echo "nome: ".$_POST['nome']."<br><br>";
+echo "email: ".$_POST['email'];
+?>
+</body>
+</html>
